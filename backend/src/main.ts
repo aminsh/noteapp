95edd04a25cf03d45da30926678754f1c5f3d3f4
amin sheikhi <amin_s@felixin.io>
2024-04-04 11:26:38 +0330

    style(App): normal

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import { join } from 'path';
+import { join } from 'path'
 import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
