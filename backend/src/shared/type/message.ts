1e4159ac49887195dace49b8d853e13e503d029a
amin sheikhi <amin_s@felixin.io>
2024-06-02 19:56:20 +0330

    style(App): normal

--- backend/src/shared/type/message.ts
+++ backend/src/shared/type/message.ts
@@ -1,6 +1,5 @@
 import { Observable } from 'rxjs'
 import { AuthenticatedUser } from '../../user/user.type'
-import { ClientProxy } from '@nestjs/microservices'
 
 export interface MessagePayload<TBody> {
   headers: {user: AuthenticatedUser}
